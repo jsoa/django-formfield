63a6b775db613b2c431084a88ec46d90655e38e1
Jose Soares <jose@linux.com>
2013-02-05 20:26:19 -0500

    [-] more pep8 related changes

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from os import path
 from setuptools import setup, find_packages
+
 
 def read_file(filename):
     """Read a file into a string"""
-    path = os.path.abspath(os.path.dirname(__file__))
-    filepath = os.path.join(path, filename)
+    path = path.abspath(path.dirname(__file__))
+    filepath = path.join(path, filename)
     try:
         return open(filepath).read()
     except IOError:
         return ''
-    
+
+
 def get_readme():
     """Return the README file contents. Supports text,rst, and markdown"""
-    for name in ('README','README.rst','README.md'):
-        if os.path.exists(name):
+    for name in ('README', 'README.rst', 'README.md'):
+        if path.exists(name):
             return read_file(name)
     return ''
 
@@ -21,17 +25,17 @@ def get_readme():
 DESC = " ".join(__import__('formfield').__doc__.splitlines()).strip()
 
 setup(
-    name = "django-formfield",
-    version = __import__('formfield').get_version().replace(' ', '-'),
-    url = 'http://github.com/jsoa/django-formfield/',
-    author = 'Jose Soares',
-    author_email = 'jose@linux.com',
-    description = DESC,
-    long_description = get_readme(),
-    packages = find_packages(),
-    include_package_data = True,
-    install_requires = read_file('requirements.txt'),
-    classifiers = [
+    name="django-formfield",
+    version=__import__('formfield').get_version().replace(' ', '-'),
+    url='http://github.com/jsoa/django-formfield/',
+    author='Jose Soares',
+    author_email='jose@linux.com',
+    description=DESC,
+    long_description=get_readme(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_file('requirements.txt'),
+    classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Framework :: Django',
     ],
